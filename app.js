c4767c6ec6305ae001fe50a0f10fac1780e77548
AmerAbuZaiton <>
2021-07-01 19:31:45 +0000

    new html

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-alert("Welcome to my website ")
+/* alert("Welcome to my website ")
 
 var typeCat =prompt("Enter your cat's type from American ,Balinese,Chartreux ")
 if(typeCat == "American"){alert("There is some information about this type of cat")}
@@ -13,3 +12,29 @@ if(catWeigth >299 && catWeigth <501)
 else{alert("Your cat needs more care  perfect weight between 300 to 500")}
 var catName=prompt("Enter your cat name")
 document.write("<h3>"+ catName+ "</h3>")
+ */
+ alert("Welcome");
+var userType = prompt("Enter if you admin or user")
+  if (userType == "admin") {
+
+  var adminName = prompt("Enter Your UserName")
+  if (adminName == "ahmad" || adminName || == "Amer" adminName || == "osama")
+  {
+    var pass = prompt("Enter your password")
+    if (adminName == "ahmad" && pass == "12345") {
+      document.write("<h2>" + "Welcome " + adminName + "</h2>")
+    }
+    else if (adminName == "amer" && pass == "678910") {
+      document.write("<h2>" + "Welcome " + adminName + "</h2>")
+    }
+
+  }else if (adminName == "osama" && pass == "54321") {
+    document.write("<h2>" + "Welcome " + adminName + "</h2>")
+  } else { alert("password invalid") }
+
+} else { alert("User name Invalid") }
+
+}
+
+else { alert("welcom ") 
+  */
