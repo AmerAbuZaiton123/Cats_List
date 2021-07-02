f36644e4601898eee0c566008a5423663b7a5de4
AmerAbuZaiton <>
2021-07-02 19:05:43 +0000

    create op-Loop-summery file

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ if(catWeigth >299 && catWeigth <501)
 else{alert("Your cat needs more care  perfect weight between 300 to 500")}
 var catName=prompt("Enter your cat name")
 document.write("<h3>"+ catName+ "</h3>")
- */
- alert("Welcome");
-var userType = prompt("Enter if you admin or user")
-  if (userType == "admin") {
-
+*/
+/*  alert("Welcome");
+ var userType = prompt("Enter if you admin or user")
+  if (  "admin" == userType) {
+  
   var adminName = prompt("Enter Your UserName")
   if (adminName == "ahmad" || adminName || == "Amer" adminName || == "osama")
   {
@@ -37,4 +37,38 @@ var userType = prompt("Enter if you admin or user")
 }
 
 else { alert("welcom ") 
-  */
+  *//* 
+var evensum=0
+var oddsum=0
+
+var num=prompt("Enter Naumber to detrmain how match even and odd number")
+ 
+for(var i=0 ;i < num;i++)
+{
+  if(i %2==0)
+  { 
+     evensum + 1
+    document.write("<h4>"+""i+""+"</h4>")
+  }/
+  else
+*/
+
+alert("Welcome")
+var catType = prompt("Enter the type of cat from the list below \n American Curl ,Chartreux ,Balinese ")
+
+while (catType != 'americancurl' && catType != 'chartreux' && catType != 'balinese') {
+  catType = prompt("please select from the list")
+}
+
+var numberRe = prompt(' How ' + catType + 'do you want to see ?')
+
+
+for (var i = 0; i <= numberRe; i++) {
+  if(catType == "americancurl") {
+    document.write('<img src="https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/american-curl-detail.jpg?bust=1535566890&width=355" alt="American Curl photo">')
+  } else if(catType == "balinese") { document.write('<img src="https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/balinese-detail.jpg?bust=1535566910&width=355" alt="Balinese">')}
+ else
+ {
+  document.write(' <img src="https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/chartreux-detail.jpg?bust=1535566975&width=355" alt="Chartreux">')
+ }
+}
